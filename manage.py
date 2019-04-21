855fefdce5bdcebc96184572a54ad7ff3962fd4c
YusupovAI <iusupov.ai@phystech.edu>
2019-04-21 23:32:17 +0300

    testing

--- manage.py
+++ manage.py
@@ -1,28 +1,39 @@
-from config import bot_token
 import os
+
 from flask import Flask, request
+from config import bot_token
+
 import telebot
-from app import bot, init_bot
 
-
+TOKEN = bot_token
+bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 
-@server.route('/' + bot_token, methods=['POST'])
-def get_message():
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
     bot.process_new_updates(
         [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
 
 @server.route("/")
-def web_hook():
+def webhook():
     bot.remove_webhook()
     bot.set_webhook(
-        url='https://python-review-test-bot.herokuapp.com/' + bot_token)
+        url='https://python-review-test-bot.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
 if __name__ == "__main__":
-    init_bot(bot_token)
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
