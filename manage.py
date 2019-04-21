a8f1b0b7f54c40ddb9836b2c360e43569da816a7
YusupovAI <iusupov.ai@phystech.edu>
2019-04-21 23:14:07 +0300

    fixed bugs

--- manage.py
+++ manage.py
@@ -2,8 +2,9 @@ from config import bot_token
 import os
 from flask import Flask, request
 import telebot
+from app import bot, init_bot
 
-bot = telebot.TeleBot(bot_token)
+
 server = Flask(__name__)
 
 
@@ -18,9 +19,10 @@ def get_message():
 def web_hook():
     bot.remove_webhook()
     bot.set_webhook(
-        url='https://python-review-test-bot.herokuapp.com')
+        url='https://python-review-test-bot.herokuapp.com/' + bot_token)
     return "!", 200
 
 
 if __name__ == "__main__":
+    init_bot(bot_token)
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
