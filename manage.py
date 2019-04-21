16a651abd890b57da7f9e4d1d66f5cb504e6e7b0
YusupovAI <iusupov.ai@phystech.edu>
2019-04-21 21:03:05 +0300

    Heroku

--- manage.py
+++ manage.py
@@ -7,8 +7,6 @@ port = int(os.environ.get('PORT', '8443'))
 
 def main():
     app.init_bot(config.bot_token)
-    app.bot.start_webhook(listen='0.0.0.0', port=port,
-                          url_path=config.bot_token)
     app.bot.set_webhook(
         "https://python-review-test-bot.herokuapp.com/" + config.bot_token)
     app.bot.idle()
