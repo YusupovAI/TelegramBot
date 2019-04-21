98534d4214b39e3cbf83b076cc9d40fa2dbb5d79
YusupovAI <iusupov.ai@phystech.edu>
2019-04-21 22:56:20 +0300

    added flask

--- manage.py
+++ manage.py
@@ -1,16 +1,27 @@
-import app
-import config
+from config import bot_token
 import os
+from flask import Flask, request
+import telebot
+from app import bot, init_bot
 
 port = int(os.environ.get('PORT', '8443'))
+init_bot(bot_token)
+server = Flask(__name__)
 
 
-def main():
-    app.init_bot(config.bot_token)
-    app.bot.set_webhook(
-        "https://python-review-test-bot.herokuapp.com/" + config.bot_token)
-    app.bot.idle()
+@server.route('/' + bot_token, methods=['POST'])
+def getMessage():
+    bot.process_new_updates(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 
-if __name__ == '__main__':
-    main()
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://your_heroku_project.com/' + bot_token)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
