bc22e18bf85eccda3504ca07dc0d048a9024e3b0
YusupovAI <iusupov.ai@phystech.edu>
2019-04-21 23:31:17 +0300

    testing

--- app/handlers.py
+++ app/handlers.py
@@ -1,39 +1,80 @@
-import os
-
-from flask import Flask, request
-from config import bot_token
-
+from app import bot, chatdb
+import requests
 import telebot
-
-TOKEN = bot_token
-bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
+import config
+from app import chatdb
 
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+def handle_start(message):
+    bot.send_message(message.chat.id,
+                     'Hello! I am a bot for searching science articles. '
+                     'Just use /search command to begin')
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+def get_articles(response):
+    l = []
+    for publication in response.json()['data']:
+        source = publication['_source']
+        result = dict()
+        result['authors'] = source['authors']
+        result['date'] = source['datePublished']
+        result['title'] = source['title']
+        result['urls'] = source['urls']
+        l.append(result)
+    return l
 
 
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates(
-        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+def formulate_text(article):
+    return '''Title: {title}
+    
+authors: {authors}
+
+date: {date}'''.format(title=article['title'],
+                       authors=', '.join(article['authors']),
+                       date=article['date'])
 
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(
-        url='https://python-review-test-bot.herokuapp.com/' + TOKEN)
-    return "!", 200
+def create_keyboard():
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    key_next = telebot.types.InlineKeyboardButton('Next',
+                                                  callback_data='next')
+    key_get = telebot.types.InlineKeyboardButton('Get sources',
+                                                 callback_data='get')
+    keyboard.add(key_get)
+    keyboard.add(key_next)
+    return keyboard
 
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+def next_article(chat_id):
+    article = chatdb.get_article_for_user(chat_id)
+    if article is None:
+        bot.send_message(chat_id, text='Sorry, variants ended')
+    else:
+        bot.send_message(chat_id, text=formulate_text(article),
+                         reply_markup=create_keyboard())
+
+
+@bot.message_handler(commands=['search'])
+def handle_search(message):
+    query = ' '.join(message.text.split()[1:])
+    response = requests.get(config.search_url.format(query=query),
+                            params=config.params)
+    chat_id = message.chat.id
+    if response.json()['data'] is None:
+        bot.send_message(chat_id, 'Sorry, nothing found')
+    else:
+        chatdb.set_user_articles(chat_id, get_articles(response))
+        next_article(chat_id)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == 'next':
+        next_article(call.message.chat.id)
+    elif call.data == 'get':
+        bot.send_message(call.message.chat.id,
+                         '{urls}'.format(
+                             urls='\n'.join(
+                                 chatdb.get_last_urls(call.message.chat.id))))
+        chatdb.del_user_articles(call.message.chat.id)
