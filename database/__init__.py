5fc9cdb98aeed350b2dbb2d36217b056b9e0ba84
YusupovAI <iusupov.ai@phystech.edu>
2019-04-29 10:02:18 +0300

    Added database

--- database/__init__.py
+++ database/__init__.py
@@ -1,7 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
 
 engine = create_engine('sqlite:///:memory:', echo=False)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
+
+
+@contextmanager
+def get_session():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+    finally:
+        session.close()
