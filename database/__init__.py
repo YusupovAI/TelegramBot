3acb7c5c107f358a799d3b0b5d854f7ff7b1386b
YusupovAI <iusupov.ai@phystech.edu>
2019-04-29 10:19:46 +0300

    Added logs

--- database/__init__.py
+++ database/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
 
-engine = create_engine('sqlite:///:memory:', echo=False)
+engine = create_engine('sqlite:///:memory:', echo=True)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
