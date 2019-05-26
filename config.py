57b78f425d4ca6a15c28627d00f9da481039e5f2
YusupovAI <iusupov.ai@phystech.edu>
2019-05-26 14:37:11 +0300

    Fixed config

--- config.py
+++ config.py
@@ -1,25 +1,9 @@
 from json import load
 
-BOT_TOKEN = None
-API_KEY = None
-SEARCH_URL = None
-PARAMS = None
-PORT = None
-HOST = None
-DB_NAME = 'users.db'
-
-
-def init_config():
-    global BOT_TOKEN
-    global SEARCH_URL
-    global PARAMS
-    global PORT
-    global HOST
-    global DB_NAME
-    with open('config.json', 'r') as file:
-        params = load(file)
-        BOT_TOKEN = params['BOT_TOKEN']
-        SEARCH_URL = params['SEARCH_URL']
-        HOST = params['HOST']
-        PORT = params['PORT']
-        DB_NAME = params['DB_NAME']
+with open('config.json', 'r') as file:
+    params = load(file)
+    BOT_TOKEN = params['BOT_TOKEN']
+    SEARCH_URL = params['SEARCH_URL']
+    HOST = params['HOST']
+    PORT = params['PORT']
+    DB_NAME = params['DB_NAME']
--- manage.py
+++ manage.py
@@ -1,32 +1,28 @@
 import os
 from app import bot
 from flask import Flask, request
-from config import init_config
+from config import BOT_TOKEN, PORT, HOST
 import telebot
 from database import init_db
 
+server = Flask(__name__)
+
+
+@server.route('/' + BOT_TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(
+        url='https://python-review-test-bot.herokuapp.com/' + BOT_TOKEN)
+    return "!", 200
+
+
 if __name__ == "__main__":
-    init_config()
-    from config import BOT_TOKEN, HOST, PORT
-
-    server = Flask(__name__)
-
-
-    @server.route('/' + BOT_TOKEN, methods=['POST'])
-    def get_message():
-        bot.process_new_updates(
-            [telebot.types.Update.de_json(
-                request.stream.read().decode("utf-8"))])
-        return "!", 200
-
-
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(
-            url='https://python-review-test-bot.herokuapp.com/' + BOT_TOKEN)
-        return "!", 200
-
-
     init_db()
     server.run(host=HOST, port=int(os.environ.get('PORT', PORT)))
