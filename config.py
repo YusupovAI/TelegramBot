68cba5346bcaa3915991b6a0076dc37717d8643b
YusupovAI <iusupov.ai@phystech.edu>
2019-05-26 14:39:59 +0300

    Fixed config

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from json import load
 with open('config.json', 'r') as file:
     params = load(file)
     BOT_TOKEN = params['BOT_TOKEN']
+    PARAMS = params['PARAMS']
     SEARCH_URL = params['SEARCH_URL']
     HOST = params['HOST']
     PORT = params['PORT']
